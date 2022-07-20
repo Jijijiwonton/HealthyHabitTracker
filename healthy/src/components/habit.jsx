baa3cf5c00bebf3bdc276e621ad71d1d1eb4bff3
jiwonhwang <jijijiwonton@github.com>
2022-07-19 22:09:04 -0400

    Jiwon | increaseHours method implemented

---
+++ healthy/src/components/habit.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Habit extends Component {
+  render() {
+    return (
+      <>
+        <span className="habit-name"></span>;
+        <span className="habit-count"></span>;
+      </>
+    );
+  }
+}
+
+export default Habit;
